8274dd772858aa0e7d22f00f088c6467986dd0ac
yangch <yangchaohui@live.cn>
2021-04-03 01:32:27 +0800

    3

--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@ async function handleRequest(request: Request) {
     // Check if the request is for style.css.
     var exts = ['.html', '.css', '.js']
     if (exts.some((ext) => pathname.endsWith(ext))) {
-        const style = new URL('/static'+pathname, import.meta.url)
+        const style = new URL('static'+pathname, import.meta.url)
         return new Response(JSON.stringify(style))
         console.log(style)
         const response = await fetch(style)
