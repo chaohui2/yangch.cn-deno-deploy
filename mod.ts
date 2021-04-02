364cdeb9c129ed9419319c6a7d9cc44940f6ab85
yangch <yangchaohui@live.cn>
2021-04-03 01:19:07 +0800

    加入静态资源代理

--- mod.ts
+++ mod.ts
@@ -1,27 +1,25 @@
+import mime from "https://raw.githubusercontent.com/broofa/mime/master/index.js";
 async function handleRequest(request: Request) {
     const { pathname } = new URL(request.url);
-  
+
     // This is how the proxy works:
     // 1. A request comes in for a specific asset.
     // 2. We construct a URL to that asset.
     // 3. We fetch the asset and respond to the request.
-  
+
     // Check if the request is for style.css.
-    if (pathname.startsWith("/style.css")) {
-      //  Construct a new URL to style.css by using the URL
-      //  of the script (mod.ts) as base (import.meta.url).
-      const style = new URL("style.css", import.meta.url);
-      const response = await fetch(style);
-      // Set the appropriate content-type header value.
-      response.headers.set("content-type", "text/css; charset=utf-8");
-      // Return the response with modified content-type header.
-      return response;
-    }else if (pathname.startsWith("/test1")) {
-        return new Response(import.meta.url);
-      }
-  
+    var exts = ['.html', '.css', '.js']
+    if (exts.some((ext) => pathname.endsWith(ext))) {
+        const style = new URL(pathname.split('/').reverse()[0], import.meta.url+'/static/')
+        const response = await fetch(style)
+        response.headers.set("content-type",mime.getType(pathname)+";charset=utf-8")
+        return response;
+    } else if (pathname.startsWith("/test1")) {
+        return new Response(import.meta.url)
+    }
+
     return new Response(
-      `<html>
+        `<html>
         <head>
           <link rel="stylesheet" href="style.css" />
         </head>
@@ -29,14 +27,14 @@ async function handleRequest(request: Request) {
           <h1>Example</h1>
         </body>
       </html>`,
-      {
-        headers: {
-          "content-type": "text/html; charset=utf-8",
+        {
+            headers: {
+                "content-type": "text/html; charset=utf-8",
+            },
         },
-      },
     );
-  }
-  
-  addEventListener("fetch", (event: FetchEvent) => {
+}
+
+addEventListener("fetch", (event: FetchEvent) => {
     event.respondWith(handleRequest(event.request));
-  });
+});
